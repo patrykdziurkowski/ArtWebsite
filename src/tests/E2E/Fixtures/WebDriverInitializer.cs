500cac11aa6cb0f82bb01e3d24b257b03bb1ff46
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2025-02-14 15:38:40 +0100

    Fixed a bug where browser would show up during selenium tests when it shouldnt.

--- src/tests/E2E/Fixtures/WebDriverInitializer.cs
+++ src/tests/E2E/Fixtures/WebDriverInitializer.cs
@@ -16,13 +16,11 @@ public class WebDriverInitializer : IDisposable
                 ChromeOptions options = new();
                 // Headless if variable is not set or set to true. The Selenium browser window opens otherwise.
                 DotEnv.Load("../../../../../.env"); // Load variables from .env file
-                string? headlessString = Environment.GetEnvironmentVariable("SELENIUM_HEADLESS");
-                if (bool.TryParse(headlessString, out bool isHeadless))
+                string? headlessVariable = Environment.GetEnvironmentVariable("SELENIUM_HEADLESS");
+                bool isParsed = bool.TryParse(headlessVariable, out bool isHeadless);
+                if (headlessVariable is null || isParsed == false || isHeadless)
                 {
-                        if (headlessString is null || isHeadless)
-                        {
-                                options.AddArguments("--headless=new");
-                        }
+                        options.AddArguments("--headless=new");
                 }
                 options.AddArguments("--no-sandbox");
                 options.AddArguments("--disable-dev-shm-usage");
