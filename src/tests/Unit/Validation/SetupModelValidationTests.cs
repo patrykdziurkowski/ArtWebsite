75abb70c4fe5783a847e3079970e954ae09b8a51
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2025-02-20 17:53:22 +0100

    Added name validation for artist's SetupModel.

--- src/tests/Unit/Validation/SetupModelValidationTests.cs
+++ src/tests/Unit/Validation/SetupModelValidationTests.cs
@@ -10,6 +10,30 @@ public class SetupModelValidationTests : ValidationBase
         [Theory]
         [InlineData("")]
         [InlineData(null)]
+        [InlineData("ab")]
+        [InlineData("abcdefghijklm")]
+        [InlineData("Some User")]
+        [InlineData(" SomeUser")]
+        [InlineData("SomeUser ")]
+        [InlineData("Some\tUser")]
+        [InlineData("SomeUser!")]
+        [InlineData("SomeUser😄")]
+        public void Validation_ShouldFail_WhenNameInvalid(string? name)
+        {
+                SetupModel model = new()
+                {
+                        Name = name!,
+                        Summary = "This is a summary of my profile!",
+                };
+
+                IList<ValidationResult> validationErrors = Validate(model);
+
+                validationErrors.Should().HaveCount(1);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
         public void Validation_ShouldFail_WhenSummaryNotProvided(string? summary)
         {
                 SetupModel model = new()
