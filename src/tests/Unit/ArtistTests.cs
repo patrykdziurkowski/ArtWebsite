88f1d6b2a1fd161e28e5e8165b68bcb9fc556832
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2025-03-16 15:45:24 +0100

    Added a method to boost an art piece.

---
+++ src/tests/Unit/ArtistTests.cs
@@ -0,0 +1,77 @@
+using FluentAssertions;
+using FluentResults;
+using web.Features.Artists;
+using web.Features.ArtPieces;
+
+namespace tests.Unit;
+
+public class ArtistTests
+{
+        [Fact]
+        public void BoostArtPiece_ShouldReturnFail_WhenBoostAlreadyActive()
+        {
+                Artist artist = new()
+                {
+                        Name = "SomeArtist",
+                        Summary = "My profile summary.",
+                        UserId = Guid.NewGuid()
+                };
+                ArtPiece artPiece = new()
+                {
+                        ImagePath = "testPath",
+                        Description = "some description",
+                        ArtistId = artist.Id,
+                };
+                Result firstResult = artist.BoostArtPiece(artPiece.Id, artist.Id);
+
+                Result secondResult = artist.BoostArtPiece(artPiece.Id, artist.Id);
+
+                firstResult.IsSuccess.Should().BeTrue();
+                secondResult.IsFailed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void BoostArtPiece_ShouldReturnFail_WhenArtPieceNotOwnedByCurrentArtist()
+        {
+                Artist artist = new()
+                {
+                        Name = "SomeArtist",
+                        Summary = "My profile summary.",
+                        UserId = Guid.NewGuid()
+                };
+                ArtPiece artPiece = new()
+                {
+                        ImagePath = "testPath",
+                        Description = "some description",
+                        ArtistId = new ArtistId(),
+                };
+
+                Result result = artist.BoostArtPiece(artPiece.Id, artPiece.ArtistId);
+
+                result.IsFailed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void BoostArtPiece_ShouldReturnOkAndAddBoost_WhenValid()
+        {
+                Artist artist = new()
+                {
+                        Name = "SomeArtist",
+                        Summary = "My profile summary.",
+                        UserId = Guid.NewGuid()
+                };
+                ArtPiece artPiece = new()
+                {
+                        ImagePath = "testPath",
+                        Description = "some description",
+                        ArtistId = artist.Id,
+                };
+
+                Result result = artist.BoostArtPiece(artPiece.Id, artist.Id);
+
+                result.IsSuccess.Should().BeTrue();
+                artist.ActiveBoost.Should().NotBeNull();
+        }
+
+
+}
--- src/web/Features/Artists/Artist.cs
+++ src/web/Features/Artists/Artist.cs
@@ -1,3 +1,5 @@
+using FluentResults;
+using web.Features.ArtPieces;
 using web.Features.Shared.domain;
 
 namespace web.Features.Artists;
@@ -9,4 +11,24 @@ public class Artist : AggreggateRoot
         public required string Summary { get; set; }
         public Boost? ActiveBoost { get; private set; }
         public required Guid UserId { get; init; }
+
+        public Result BoostArtPiece(ArtPieceId artPieceId, ArtistId artPieceArtistId)
+        {
+                if (artPieceArtistId != Id)
+                {
+                        return Result.Fail("You may not boost another artist's art piece.");
+                }
+
+                if (ActiveBoost is not null && ActiveBoost.IsActive)
+                {
+                        return Result.Fail("An art piece is already boosted.");
+                }
+
+                ActiveBoost = new()
+                {
+                        ArtistId = Id,
+                        ArtPieceId = artPieceId,
+                };
+                return Result.Ok();
+        }
 }
