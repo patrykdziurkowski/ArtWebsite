dfdb43de7bca7da73648a8ec94678454e12cc9f9
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2025-12-07 20:53:53 +0100

    Add missions for liking art pieces.

--- src/tests/Integration/Commands/LikeArtPieceCommandTests.cs
+++ src/tests/Integration/Commands/LikeArtPieceCommandTests.cs
@@ -1,8 +1,10 @@
 using FluentAssertions;
 using FluentResults;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using tests.Integration.Fixtures;
 using web.Features.ArtPieces;
+using web.Features.Missions;
 using web.Features.Reviewers;
 using web.Features.Reviewers.LikeArtPiece;
 
@@ -12,10 +14,15 @@ public class LikeArtPieceCommandTests : DatabaseTest
 {
         private readonly LikeArtPieceCommand _command;
 
+        private readonly IMissionGenerator _mockMissionGenerator;
+
         public LikeArtPieceCommandTests(DatabaseTestContext databaseContext)
                 : base(databaseContext)
         {
-                _command = Scope.ServiceProvider.GetRequiredService<LikeArtPieceCommand>();
+                ReviewerRepository reviewerRepository = Scope.ServiceProvider.GetRequiredService<ReviewerRepository>();
+                _mockMissionGenerator = Substitute.For<IMissionGenerator>();
+                MissionManager missionManager = new(DbContext, _mockMissionGenerator);
+                _command = new(reviewerRepository, missionManager);
         }
 
         [Fact]
@@ -48,5 +55,23 @@ public class LikeArtPieceCommandTests : DatabaseTest
                 await _command.ExecuteAsync(currentUserId, artPieceIds.First());
 
                 DbContext.Likes.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task Execute_ShouldAddExtraPoints_WhenMissionCompleted()
+        {
+                _mockMissionGenerator.GetMissions(Arg.Any<Guid>(), Arg.Any<DateTimeOffset>(), 1)
+                        .Returns([MissionType.LikeArt]);
+                int numberOfLikesForMission = MissionType.LikeArt.GetMaxProgressCount();
+                List<ArtPieceId> artPieceIds = await CreateArtistUserWithArtPieces();
+                Guid currentUserId = DbContext.Users.First().Id;
+
+                for (int i = 0; i < numberOfLikesForMission; i++)
+                {
+                        await _command.ExecuteAsync(currentUserId, artPieceIds[i]);
+                }
+
+                DbContext.Reviewers.Single(r => r.UserId == currentUserId).Points.Should().Be(25);
+                DbContext.Likes.Should().HaveCount(numberOfLikesForMission);
         }
 }
--- src/web/Features/Reviewers/LikeArtPiece/LikeArtPieceCommand.cs
+++ src/web/Features/Reviewers/LikeArtPiece/LikeArtPieceCommand.cs
@@ -1,13 +1,20 @@
 using FluentResults;
 using web.Features.ArtPieces;
+using web.Features.Missions;
 
 namespace web.Features.Reviewers.LikeArtPiece;
 
-public class LikeArtPieceCommand(ReviewerRepository reviewerRepository)
+public class LikeArtPieceCommand(
+        ReviewerRepository reviewerRepository,
+        MissionManager missionManager)
 {
-        public async Task<Result<Like>> ExecuteAsync(Guid currentUserId,
-                ArtPieceId artPieceId)
+        public async Task<Result<Like>> ExecuteAsync(
+                Guid currentUserId,
+                ArtPieceId artPieceId,
+                DateTimeOffset? now = null)
         {
+                now ??= DateTimeOffset.UtcNow;
+
                 Reviewer? reviewer = await reviewerRepository.GetByIdAsync(currentUserId);
                 if (reviewer is null)
                 {
@@ -21,6 +28,9 @@ public class LikeArtPieceCommand(ReviewerRepository reviewerRepository)
                 }
 
                 await reviewerRepository.SaveAsync(reviewer);
+
+                await missionManager.RecordProgressAsync(MissionType.LikeArt, currentUserId, now.Value);
+
                 return Result.Ok(reviewer.ActiveLikes.Last());
         }
 }
