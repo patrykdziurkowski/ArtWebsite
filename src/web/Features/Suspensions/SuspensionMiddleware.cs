fb9d1a83e23b17f5839782f338923f417382be34
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2026-02-22 10:13:28 +0100

    Fix search bar and daily missions being visible for suspended user, causing JS API errors due to being unauthorized.

--- src/web/Features/Suspensions/SuspensionMiddleware.cs
+++ src/web/Features/Suspensions/SuspensionMiddleware.cs
@@ -24,20 +24,21 @@ public class SuspensionMiddleware(RequestDelegate next)
                         return;
                 }
 
-                PathString path = context.Request.Path;
+                Guid userId = Guid.Parse(context.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+                Suspension? activeSuspension = await dbContext.Suspensions
+                        .Where(s => s.UserId == userId && s.ExpiryDate > DateTimeOffset.UtcNow)
+                        .OrderByDescending(s => s.ExpiryDate)
+                        .FirstOrDefaultAsync();
 
+                context.Items["IsSuspended"] = activeSuspension is not null;
+
+                PathString path = context.Request.Path;
                 if (AllowedPaths.Any(path.StartsWithSegments))
                 {
                         await _next(context);
                         return;
                 }
 
-                Guid userId = Guid.Parse(context.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-
-                Suspension? activeSuspension = await dbContext.Suspensions
-                        .Where(s => s.UserId == userId && s.ExpiryDate > DateTimeOffset.UtcNow)
-                        .OrderByDescending(s => s.ExpiryDate)
-                        .FirstOrDefaultAsync();
                 if (activeSuspension is not null)
                 {
                         context.Response.Redirect("/Suspended");
